b043374e0f11b3b26aced80a4ba474830032b091
luis leon <leonluis380@yahoo.com>
2021-06-13 12:31:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,14 @@
 class Shoe
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
+
   def initialize(brand)
     @brand = brand
-
-
   end
+
+  def cobble
+    puts "The shoe has been repaired"
+    @condition = "new"
+  
 
 end
